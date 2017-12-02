3956567b5a8d7eb51155819a68410c47f5cebe47
blitzingeagle <mcmorrischen@gmail.com>
2017-12-01 22:51:12 -0500

    Fix server code

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
-var http = require(“http”);
+var http = require("http");
 
 http.createServer(function(req, res) {
-    res.write(“Hello World!);
+    res.write("Hello World!");
     res.end();
 }).listen(8080);
-
