b48e38eb7121133e73d98451e0d7b2f65a15d234
blitzingeagle <mcmorrischen@gmail.com>
2017-12-02 15:47:40 -0500

    post data

--- server/mongo_queries.js
+++ server/mongo_queries.js
@@ -3,9 +3,9 @@ function new_post(db, data, callback) {
 }
 
 function post_data(db, data, callback) {
-  
+  db.collection("post_data").insertMany(data, callback);
 }
 
 module.exports.mongo_queries = {
-  new_post
+  new_post, post_data
 }
