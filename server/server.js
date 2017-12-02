b31ddd516ac8c3ab65cbbc6e05f54ab4634aa4cd
blitzingeagle <mcmorrischen@gmail.com>
2017-12-02 01:19:00 -0500

    Express js

--- server/server.js
+++ server/server.js
@@ -1,48 +1,37 @@
 var http = require("http");
 var fs = require("fs");
 var url = require('url');
+var express = require("express");
+var myParser = require("body-parser");
 
 var port = 4242;
 
-var server = http.createServer(function(req, res) {
-  if (req.url === "/favicon.ico") {
-    res.writeHead(200, {'Content-Type': 'image/x-icon'} );
-    return;
-  }
+var app = express();
+app.use(myParser.urlencoded({extended : true}));
 
+app.post("/", function(req, res) {
   var request_url = url.parse(req.url, true);
+  console.log("Incoming POST request to " + request_url.pathname + " from " + req.connection.remoteAddress);
 
-  if(request_url.pathname === "/") {
-    console.log("Incoming " + req.method + " request from " + req.connection.remoteAddress);
+  res.writeHead(200, {"Content-Type": "text/html"});
+  res.end("POST received");
 
-    if(req.method === "POST") {
-      var body = "";
-      req.on('data', function(data) {
-          body += data;
-          console.log("Partial body: " + body);
-      });
-      req.on('end', function() {
-          console.log("Body: " + body);
-      });
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.end('POST received');
-    } else if(req.method === "GET") {
-      var html = fs.readFileSync('index.html');
-      var query = request_url.query;
-      console.log(query);
-
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(html);
-      res.end("GET received with data: " + JSON.stringify(query));
-    } else {
-      console.log("Unsupported request: " + req.method);
-      res.end();
-    }
-
-  } else {
-    res.end("No receiver for " + request_url.pathname);
-  }
+  console.log(req.body);
 });
 
-server.listen(port);
+app.get("/", function(req, res) {
+  var request_url = url.parse(req.url, true);
+  console.log("Incoming GET request to " + request_url.pathname + " from " + req.connection.remoteAddress);
+
+  var html = fs.readFileSync('index.html');
+  var query = request_url.query;
+
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(html);
+  res.end("GET received with data: " + JSON.stringify(query));
+
+  console.log(query);
+});
+
+app.listen(port);
 console.log("Server listening on port " + port);
